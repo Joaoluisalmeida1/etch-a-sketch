232765c0085fd549a356e1253d8385a239db900a
João Almeida <joaoluisalmeida98@gmail.com>
2024-07-16 22:43:32 +0100

    Added button to reset code, added logic to only paint while clicking

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-
-    // to create a 16x16 grid, we need to first get the container
-    const container = document.querySelector(".container");
+    function makeUserInput () {
+        let input;
+        do {
+            input = prompt("How big would you like ths grid to be? The maximum is 100.");
+        } while (isNaN(input) || input < 1 || input > 100);
+        return parseInt(input, 10);
+    };
 
     // need to create a function, makeGrid, that generates i rows and j columns as divs
-    function makeGrid(gridSize) {        
-        for (let i = 0; i < gridSize; i++) {
-            let column = document.createElement("div");
-            column.classList.add("column");
-            for (let j = 0; j < gridSize; j++) {
-                let row = document.createElement("div");
-                row.classList.add("row");
-                row.style.border = "2px solid black";
-                row.innerText = `${j+1}${1+i}`;
-                column.appendChild(row);
-            }
-            container.appendChild(column);
+    function makeGrid(gridSize) { 
+        const grid = document.querySelector(".grid");
+        grid.innerHTML = "";
+        grid.style.display = "grid";
+        grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+        for (let i = 0; i < gridSize * gridSize; i++) {
+            let cell = document.createElement("div");
+            cell.classList.add("row");
+            grid.appendChild(cell);
         }
+
+        addPaintingListeners();
     }
-    makeGrid(16);
-
-
-    const squares = document.querySelectorAll(".row");
     
-    squares.forEach(square => {
-       square.addEventListener("mouseenter", (event) => {
-        event.target.style.backgroundColor = "purple";
-       });
+    function addPaintingListeners() {
+        const squares = document.querySelectorAll(".row");
+        squares.forEach(square => {
+            square.addEventListener("mouseenter", (event) => {
+            if (isMouseDown) {
+                event.target.style.backgroundColor = "pink";
+                }
+            });
+
+            square.addEventListener("mousedown", (event) => {
+                event.target.style.backgroundColor = "pink";
+            });
+        });
+    }
+
+
+    let isMouseDown = false;
+
+    document.addEventListener("mousedown", () => {
+        isMouseDown = true;
     });
 
+    document.addEventListener("mouseup", () => {
+        isMouseDown = false;
+    });
 
+    const gridButton = document.getElementById("gridButton")
+    gridButton.addEventListener("click", () => {
+        const gridSize = makeUserInput();
+        makeGrid(0);
+        makeGrid(gridSize);
+    });
+
+    const initialGridSize = makeUserInput();
+    makeGrid(initialGridSize);
 });
